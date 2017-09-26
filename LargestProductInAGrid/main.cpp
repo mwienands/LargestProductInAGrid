a9bf8005554ced15b86241b56cc67daf2fd86ac1
mwienands <mwienands@gmail.com>
2017-09-26 12:43:20 -0500

    Initial Commit

---
+++ LargestProductInAGrid/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LargestProductInAGrid
+//
+//  Created by Michael Wienands on 9/26/17.
+//  Copyright © 2017 Michael Wienands. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
